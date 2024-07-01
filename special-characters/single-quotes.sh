c5d64144579c3c40e02f6416874c878d54eea921
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-07-01 10:05:13 +0700

    refactor: add single quotes usage

---
+++ special-characters/single-quotes.sh
@@ -0,0 +1,8 @@
+CLUB="Los Angeles Dodgers"
+
+echo "-- See the differenece below?"
+echo "Welcome to $CLUB" # Welcome to Los Angeles Dodgers
+echo 'Welcome to $CLUB' # Welcome to $CLUB
+
+echo "-- A nested single quote"
+echo 'I said: '\''Hello'\''' # I said: 'Hello'
