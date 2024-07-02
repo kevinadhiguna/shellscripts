45eac2c633030cb17362e1b04201a0bff3766a0d
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-07-02 09:23:49 +0700

    refactor: add double quotes usage in shellscript

---
+++ special-characters/double-quotes.sh
@@ -0,0 +1,6 @@
+CLUB="Los Angeles Dodgers"
+
+echo "-- Double quotes: see the differenece below?"
+echo "Welcome to $CLUB" # Welcome to Los Angeles Dodgers
+echo "Welcome to \$CLUB" # Welcome to $CLUB
+echo 'Welcome to \$CLUB' # Welcome to \$CLUB
