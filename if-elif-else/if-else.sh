a20cdae48711f53553a165b033e84e6f26b45309
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-07-05 09:33:40 +0700

    refactor: add an if-else statement

---
+++ if-elif-else/if-else.sh
@@ -0,0 +1,10 @@
+EXPERIENCE=15
+
+if [ $EXPERIENCE -gt 5 ]; then
+    echo "The employee is a senior staff"
+else
+    echo "The employee might attain a junior-level position"
+fi
+
+# Output:
+# The employee is a senior staff
