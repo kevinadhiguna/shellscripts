13fa85633f1fd784ab05a0bb7b67addca0f005ea
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-07-07 07:02:30 +0700

    refactor: print current environment

---
+++ environment.sh
@@ -0,0 +1,9 @@
+# Concepts:
+# - If you are in an interactive shell (like Bash), it will typically print the name of the shell you are using (e.g., bash, sh, zsh, etc.).
+# - If you are in a script, it will print the name of the script file.
+
+echo $0
+
+# Output:
+# - In an interactive Bash session, echo $0 might output bash.
+# - If you have a script named myscript.sh and you run ./myscript.sh, echo $0 within myscript.sh would output ./myscript.sh.
