b269203ec25fe8fadf371ff88bbfbf1f58dd0794
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-07-16 20:55:40 +0700

    refactor: call a function

---
+++ functions/echo.sh
@@ -0,0 +1,10 @@
+display_text() {
+    echo "Hi! This is an examplle of a function"
+}
+
+display_text
+# Output: Hi! This is an examplle of a function
+
+#DO NOT DO:
+# display_text()
+#Output: echo.sh:1: parse error near `\n'
