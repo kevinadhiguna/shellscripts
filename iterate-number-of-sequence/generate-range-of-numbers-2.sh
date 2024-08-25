d7f619e0d453a2d2419c1930231d38c3f0b7428c
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-08-25 07:17:17 -0700

    refactor: generate a range of numbers using a for loop

---
+++ iterate-number-of-sequence/generate-range-of-numbers-2.sh
@@ -0,0 +1,19 @@
+# Generate a range of numbers using a FOR loop
+NUMBER=11
+
+for ((k=1;k <= NUMBER;k++)); do
+    echo $k
+done
+
+# Output:
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# 11
