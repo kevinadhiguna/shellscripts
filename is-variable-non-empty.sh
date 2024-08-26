fdd17775f823b201113c7a44f6ced2ab877b5b4d
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-08-26 08:23:03 -0700

    refactor: check if a variable is non empty

---
+++ is-variable-non-empty.sh
@@ -0,0 +1,12 @@
+ID=""
+echo "ID: $ID"
+
+if [[ ! $ID = "" ]]; then
+    echo "Variable 'ID' is empty"
+else
+    echo "Variable 'ID' is not empty"
+fi
+
+# Output:
+# ID: 
+# Variable 'ID' is not empty
